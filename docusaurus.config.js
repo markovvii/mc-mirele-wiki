ef7ce16ee78539a4059379e177487fa81fb4482e
Mark <30995929+markovvii@users.noreply.github.com>
2022-07-20 04:56:48 +0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,21 +33,19 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/markovvii/mc-mirele-wiki/tree/main/',
         },
-        blog: false,
-//        blog: {
-//          showReadingTime: true,
-//          // Please change this to your repo.
-//          // Remove this to remove the "edit this page" links.
-//          editUrl:
-//            'https://github.com/markovvii/mc-mirele-wiki/tree/main/',
-//        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/markovvii/mc-mirele-wiki/tree/main/',
+        },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
